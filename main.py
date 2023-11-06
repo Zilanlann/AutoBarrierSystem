26a00533f9d1837ea90ce5f5e713e4dfe8a1c18b
Zilanlann <666zilan666@gmail.com>
2023-11-06 12:01:55 +0800

    modify main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from qfluentwidgets import MessageBox, MessageBoxBase, MessageDialog
 
 from login import *
 from window import *
@@ -17,6 +18,25 @@ class MyLogin(QMainWindow, Ui_Login):
         super(MyLogin, self).__init__(parent)
         self.setupUi(self)
 
+    def setup_ui(self):
+        # 按钮绑定函数
+        self.btn_login.clicked.connect(self.checkLogin)
+
+    def checkLogin(self):
+        username = self.UserLineEdit.text()
+        password = self.PasswordLineEdit.text()
+        result = authenticate_user(username, password)
+        if result == 1:
+            my_window.show()
+            my_login.close()
+        elif result == 0:
+            QMessageBox.information(self, "提示", "该用户不存在")
+            # MessageBox.show(self)
+            # MessageBox.nativeEvent(self, 1, "该用户不存在")
+        else:
+            # MessageBox.show(self)
+            QMessageBox.information(self, "提示", "该用户不存在")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -24,6 +44,4 @@ if __name__ == '__main__':
     my_login = MyLogin()
     my_window = MyWindow()
     my_login.show()
-    my_login.btn_login.clicked.connect(my_window.show)
-    my_login.btn_login.clicked.connect(my_login.close)
     sys.exit(app.exec())
